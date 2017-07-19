b59d475d6ab21474523c97d2fa2ea63dacc4f276
Dan Maljovec <maljovec002@gmail.com>
2017-07-19 15:49:26 -0600

    Credit for this commit goes to @PaulTalbot-INL for generating an exercise for users to try at July 2017 RAVEN Workshop. Users will need to fill in the blanks for the example in doc/workshop/exercises.

---
+++ doc/workshop/exercises/rangefinder.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+def run(self,Inputs):
+  th0_rad = self.th0 * np.pi/180.
+
+  self.tf = time_of_flight(self.v0, th0_rad, self.g)
+
+  self.R = x_position(self.v0, th0_rad, self.tf)
+
+  vx = self.v0 * np.cos(th0_rad)
+  vy = velocity_y(self.v0, th0_rad, self.g, self.tf)
+  self.vf = velocity_mag(vx,vy)
+
+def time_of_flight(v0,th0,g):
+  return 2. * v0 * np.sin(th0) / g
+
+def x_position(v0,th0,t):
+  return v0 * np.cos(th0) * t
+
+def velocity_y(v0,th0,g,t):
+  return v0*np.sin(th0) - g*t
+
+def velocity_mag(vx,vy):
+  return np.sqrt(vx*vx + vy*vy)
+
