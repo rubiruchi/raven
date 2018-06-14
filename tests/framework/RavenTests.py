228d0b8bb6a4e28b4f3c8bc7bb74e87570de30cc
Paul W. Talbot <paul.talbot@inl.gov>
2018-06-14 16:51:55 -0600

    ordered, unordered csv check

--- tests/framework/RavenTests.py
+++ tests/framework/RavenTests.py
@@ -24,6 +24,7 @@ import sys
 import pytest
 import numpy as np
 import pandas as pd
+pd.set_option('max_seq_items',7)
 
 # framework
 frameworkDir = os.path.join(os.path.dirname(__file__),'..','..','framework')
@@ -33,15 +34,14 @@ from utils import mathUtils
 
 
 class FrameworkTest(object):
-  #@pytest.fixture
-  def runRavenTest(self,fname,tests,testDir,relErr=1e-10):
+  def runRavenTest(self,fname,tests,testDir,ordered=True,relErr=1e-10):
     """
       Runs traditional framework tests.
       @ In, fname, str, name of input to run (xml file)
       @ In, tests, dict, keys are test files and gold are files to check against
       @ In, testDir, str, directory of test file
     """
-    self.__rel_err = relErr # TODO is this threadsafe?
+    self.__rel_err = relErr # TODO is this threadsafe? Also need to use it.
     raven = os.path.join(os.path.dirname(__file__),'..','..','raven_framework')
     command = raven + ' ' + os.path.abspath(fname) + ' > '+fname+'.log' # TODO better log capturing?
     os.system(command)
@@ -51,19 +51,31 @@ class FrameworkTest(object):
         gold = os.path.join('gold',test)
       test = os.path.join(testDir,test)
       gold = os.path.join(testDir,gold)
-      self._checkCSV(test,gold)
+      self._checkCSV(test,gold,ordered=ordered)
 
   #################
   #     Files     #
   #################
-  def checkFiles(self,a,b):
-    assert os.path.isfile(a)
-    assert os.path.isfile(b)
+  def checkFiles(self,*files):
+    """
+      Checks that all given files exist.
+      @ In, files, list, list of files to check
+      @ Out, None
+    """
+    for f in files:
+      assert os.path.isfile(f)
 
   #################
   #      CSV      #
   #################
-  def _checkCSV(self,testFile,goldFile):
+  def _checkCSV(self,testFile,goldFile,ordered=True):
+    """
+      Compares two CSVs for testing
+      @ In, testFile, string, absolute path of test file
+      @ In, goldFile, string, absolute path of gold file
+      @ In, ordered, bool, optional, if False then rows OR columns could be swapped (but contents consistent)
+      @ Out, None
+    """
     self.checkFiles(testFile,goldFile)
     test = self._csvLoad(testFile)
     gold = self._csvLoad(goldFile)
@@ -74,24 +86,35 @@ class FrameworkTest(object):
       return
     else:
       assert test is not None, 'Gold "{}" is not empty, but test "{}" is!'.format(goldFile,testFile)
-    # compare content and shape
-    ## compare column headers
-    diffColumns = set(gold.columns)^set(test.columns)
-    assert len(diffColumns) == 0, 'Columns have mismatched headers: "{}"'.format(', '.join(diffColumns))
-    ## check number of entries
-    assert len(gold.index) == len(test.index), 'Different number of rows: gold "{}" versus test "{}"' \
-                                               .format(len(gold.index),len(test.index))
-    # now CSVs are same shape and size and headers, so diff CSVs
-    ## align columns
-    test = test[gold.columns.tolist()]
-    ## scrub CSV for marginal values, and mark infinites
-    test = self._csvScrub(test)
-    gold = self._csvScrub(gold)
-    ## check for matching rows
-    for idx in gold.index:
-      find = gold.iloc[idx].rename(None)
-      match = self._csvFindRow(find,test)
-      assert len(match), 'No match in test "{}" for gold row "{}"!'.format(idx+1,find)
+    if ordered:
+      # while this works for ordered, it does not for unordered (as we want it)
+      ## while it has the check_like option, that searches within each column for matches,
+      ## and doesn't require one whole row to be consistent.
+      #assert gold.equals(test)
+      pd.testing.assert_frame_equal(test,gold,
+                                    check_less_precise=8,   # TODO fix for tol as input
+                                    obj='CSV')
+    else:
+      # compare content and shape
+      ## compare column headers
+      diffColumns = set(gold.columns)^set(test.columns)
+      assert len(diffColumns) == 0, 'Columns have mismatched headers: "{}"'.format(', '.join(diffColumns))
+      ## check number of entries
+      assert len(gold.index) == len(test.index), 'Different number of rows: gold "{}" versus test "{}"' \
+                                                 .format(len(gold.index),len(test.index))
+      # now CSVs are same shape and size and headers, so diff CSVs
+      ## align columns
+      test = test[gold.columns.tolist()]
+      ## scrub CSV for marginal values, and mark infinites
+      test = self._csvScrub(test)
+      gold = self._csvScrub(gold)
+      ## check for matching rows
+      for idx in gold.index:
+        find = gold.iloc[idx].rename(None)
+        match = self._csvFindRow(find,test)
+        # TODO what if there are multiple matches?
+        # TODO remove match from test (maybe if it's singular)?
+        assert len(match), 'No match in test "{}" for gold row "{}"!'.format(idx+1,find)
 
   def _csvFindRow(self,row,csv):
     """
@@ -128,9 +151,12 @@ class FrameworkTest(object):
       match = match[slice(lowest,highest+1)]
     return match
 
-
-
   def _csvLoad(self,filename):
+    """
+      Loads CSVs into pandas DataFrame objects.
+      @ In, filename, str, absolute path to file to load
+      @ Out, csv, pandas.DataFrame, csv as dataframe
+    """
     try:
       csv = pd.read_csv(filename,sep=',')
     # if file is empty, store as None
